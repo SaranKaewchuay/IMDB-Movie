c618d36cbbaf0aed78ed675b310a6ac30d285ccf
SaranKaewchuay <saranyeen@gmail.com>
2023-06-12 15:26:28 +0700

    update

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -8,27 +8,26 @@ const handleLinkClick = () => {
 const NavBar = () => {
   return (
     <nav className="navbar">
-    <div className="navbar-item">
-      <img
-        src="https://icon-library.com/images/imdb-icon-png/imdb-icon-png-15.jpg"
-        className="image-nav"
-        alt="IMDb Icon"
-      />
-    </div>
-    <ul className="navbar-list">
-      <li className="navbar-item">
-        <Link to="/" className="text-decoration-none" onClick={handleLinkClick}>
-          <button className="button">Movie</button>
-        </Link>
-      </li>
-      <li className="navbar-item">
-        <Link to="/tv-show" className="text-decoration-none" onClick={handleLinkClick}>
-          <button className="button">TV Show</button>
-        </Link>
-      </li>
-    </ul>
-  </nav>
+      <div className="navbar-item">
+        <img
+          src="https://icon-library.com/images/imdb-icon-png/imdb-icon-png-15.jpg"
+          className="image-nav"
+          alt="IMDb Icon"
+        />
+      </div>
+      <ul className="navbar-list">
+        <li className="navbar-item">
+          <Link to="/" className="text-decoration-none" >
+            <button className="button">Movie</button>
+          </Link>
+        </li>
+        <li className="navbar-item">
+          <Link to="/tv-show" className="text-decoration-none" onClick={handleLinkClick}>
+            <button className="button">TV Show</button>
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
-
 export default NavBar;
