50ac994c83cffd5b9478a213ee4f314cb3cc1046
SaranKaewchuay <saranyeen@gmail.com>
2023-06-12 15:22:06 +0700

    update

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,29 +1,33 @@
 import { Link } from "react-router-dom";
 import "../style/style.css";
 
+const handleLinkClick = () => {
+  window.location.reload();
+};
+
 const NavBar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-item">
-        <img
-          src="https://icon-library.com/images/imdb-icon-png/imdb-icon-png-15.jpg"
-          className="image-nav"
-          alt="IMDb Icon"
-        />
-      </div>
-      <ul className="navbar-list">
-        <li className="navbar-item">
-          <Link to="/" className="text-decoration-none" onClick={() => window.location.reload()}>
-            <button className="button">Movie</button>
-          </Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/tv-show" className="text-decoration-none" onClick={() => window.location.reload()}>
-            <button className="button">TV Show</button>
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <div className="navbar-item">
+      <img
+        src="https://icon-library.com/images/imdb-icon-png/imdb-icon-png-15.jpg"
+        className="image-nav"
+        alt="IMDb Icon"
+      />
+    </div>
+    <ul className="navbar-list">
+      <li className="navbar-item">
+        <Link to="/" className="text-decoration-none" onClick={handleLinkClick}>
+          <button className="button">Movie</button>
+        </Link>
+      </li>
+      <li className="navbar-item">
+        <Link to="/tv-show" className="text-decoration-none" onClick={handleLinkClick}>
+          <button className="button">TV Show</button>
+        </Link>
+      </li>
+    </ul>
+  </nav>
   );
 };
 
